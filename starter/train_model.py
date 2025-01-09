89fcc277fe3bf733acd0ecc3338945be7c85c8d9
anna <anna.pereverzeva@philips.com>
2025-01-09 18:30:05 +0300

    slice iinference + train_model reformatting

--- starter/train_model.py
+++ starter/train_model.py
@@ -6,18 +6,9 @@ import pandas as pd
 from ml.data import process_data
 import joblib
 from ml.model import train_model, inference, compute_model_metrics
+import json
 
 folder_path = os.path.dirname(os.path.abspath(__file__))
-
-# Add the necessary imports for the starter code.
-
-# Add code to load in the data.
-data = pd.read_csv(f"{folder_path}/data/census_cleaned.csv")
-
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20, random_state=42)
-
-print (train.columns)
 
 cat_features = [
     "workclass",
@@ -29,28 +20,48 @@ cat_features = [
     "sex",
     "native-country",
 ]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
 
-X_test, y_test, encoder, lb = process_data(test,categorical_features=cat_features, training = False, encoder = encoder, lb = lb,label="salary")
+def slice_inference(data, features, model, encoder, lb):
+    r = {}
+    for feature in features:
+        for value in data[feature].unique():
+            subset = data[data[feature] == value]
+            #y = subset["salary"]
+            #X = process_data(subset, encoder = encoder, lb = lb, label = None, categorical_features = features, training = False)[0]
+            X, y, _, __ = process_data(subset,categorical_features=features, training = False, encoder = encoder, lb = lb,label="salary")
+            pred = model.predict(X)
+            precision, recall, fscore = compute_model_metrics(y, pred)
+            if feature in r.keys():
+                r[feature][value] = {"precision" : precision, "recall" : recall, "fscore" : fscore}
+            else:
+                r[feature] = {}
+    return r
 
-print ("X_test.shape", X_test.shape)
-model = train_model(X_train, y_train)
-predictions = inference(model , X_test)
-print(compute_model_metrics(y_test,predictions))
 
-print (predictions)
-test["pred"] = predictions
+# Add the necessary imports for the starter code.
 
-print (test[test.pred == 0].iloc[0])
+# Add code to load in the data.
 
-print (test[test.pred == 1].iloc[0])
+if __name__ == "__main__":
 
-print (f"{folder_path}/model/")
-joblib.dump(model, f"{folder_path}/model/model.joblib")
-joblib.dump(encoder, f"{folder_path}/model/encoder.joblib")
-joblib.dump(lb, f"{folder_path}/model/lb.joblib")
+    data = pd.read_csv(f"{folder_path}/data/census_cleaned.csv")    
+    train, test = train_test_split(data, test_size=0.20, random_state=42)
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
+
+    X_test, y_test, encoder, lb = process_data(test,categorical_features=cat_features, training = False, encoder = encoder, lb = lb,label="salary")
+
+    model = train_model(X_train, y_train)
+    predictions = inference(model , X_test)
+
+    joblib.dump(model, f"{folder_path}/model/model.joblib")
+    joblib.dump(encoder, f"{folder_path}/model/encoder.joblib")
+    joblib.dump(lb, f"{folder_path}/model/lb.joblib")
+
+    r = slice_inference(test, cat_features,  model, encoder, lb)
+    with open (f"{folder_path}/model/slice_inference.json", "w") as file:
+        json.dump(r, file, indent = 4)
 
 # Proces the test data with the process_data function.
 
