9b97137d523f18f987cf5a35655df8d05b4dc118
anna <anna.pereverzeva@philips.com>
2025-01-20 00:54:07 +0100

    flake8 workflow + code cleaning

--- starter/main.py
+++ starter/main.py
@@ -1,90 +1,104 @@
 # Put the code for your API here.
-from typing import  Literal
+from typing import Literal
 import os
 from fastapi import FastAPI
 # BaseModel from Pydantic is used to define data objects.
 from pydantic import BaseModel, Field
 import pandas as pd
-import json
 import joblib
 from ml.data import process_data
-from ml.model import inference, compute_model_metrics
+from ml.model import inference
 
 folder_path = os.path.dirname(os.path.abspath(__file__))
-
 
 
 # Declare the data object with its components and their type.
 class InputItem(BaseModel):
     age: int
     workclass: Literal['State-gov', 'Self-emp-not-inc', 'Private', 'Federal-gov', 'Local-gov', '?',
-                        'Self-emp-inc', 'Without-pay', 'Never-worked']
-    #fnlgt: int
-    education: Literal['Bachelors' ,'HS-grad' ,'11th', 'Masters', '9th' ,'Some-college', 'Assoc-acdm',
-                        'Assoc-voc', '7th-8th', 'Doctorate', 'Prof-school', '5th-6th', '10th',
-                        '1st-4th', 'Preschool' ,'12th']
-    #education_num: int
-    marital_status: Literal['Never-married', 'Married-civ-spouse' ,'Divorced', 'Married-spouse-absent',
+                       'Self-emp-inc', 'Without-pay', 'Never-worked']
+    # fnlgt: int
+    education: Literal['Bachelors', 'HS-grad', '11th', 'Masters', '9th', 'Some-college', 'Assoc-acdm',
+                       'Assoc-voc', '7th-8th', 'Doctorate', 'Prof-school', '5th-6th', '10th',
+                       '1st-4th', 'Preschool', '12th']
+    # education_num: int
+    marital_status: Literal['Never-married', 'Married-civ-spouse', 'Divorced', 'Married-spouse-absent',
                             'Separated', 'Married-AF-spouse', 'Widowed']\
-                                = Field(alias="marital-status")
-    
-    occupation: Literal['Adm-clerical' ,'Exec-managerial' ,'Handlers-cleaners' ,'Prof-specialty',
+        = Field(alias="marital-status")
+
+    occupation: Literal['Adm-clerical', 'Exec-managerial', 'Handlers-cleaners', 'Prof-specialty',
                         'Other-service', 'Sales', 'Craft-repair', 'Transport-moving',
                         'Farming-fishing', 'Machine-op-inspct', 'Tech-support', '?',
- '                      Protective-serv', 'Armed-Forces', 'Priv-house-serv']
-    
-    relationship: Literal['Not-in-family', 'Husband', 'Wife', 'Own-child', 'Unmarried' ,'Other-relative']
+                        '                      Protective-serv', 'Armed-Forces', 'Priv-house-serv']
 
-    race: Literal['White', 'Black','Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other']
+    relationship: Literal['Not-in-family', 'Husband',
+                          'Wife', 'Own-child', 'Unmarried', 'Other-relative']
 
-    sex: Literal['Male' ,'Female']
+    race: Literal['White',
+                  'Black',
+                  'Asian-Pac-Islander',
+                  'Amer-Indian-Eskimo',
+                  'Other']
 
-    #capital_gain: int= Field(alias="capital-gain")
-    #capital_loss: int = Field(alias="capital-loss")
+    sex: Literal['Male', 'Female']
+
+    # capital_gain: int= Field(alias="capital-gain")
+    # capital_loss: int = Field(alias="capital-loss")
     hours_per_week: int = Field(alias="hours-per-week")
 
-    native_country: Literal['United-States' ,'Cuba', 'Jamaica' ,'India', '?', 'Mexico' ,'South',
-                            'Puerto-Rico', 'Honduras', 'England' ,'Canada', 'Germany', 'Iran',
-                            'Philippines', 'Italy', 'Poland', 'Columbia' ,'Cambodia' ,'Thailand' ,'Ecuador',
-                            'Laos', 'Taiwan' ,'Haiti', 'Portugal', 'Dominican-Republic', 'El-Salvador',
+    native_country: Literal['United-States', 'Cuba', 'Jamaica', 'India', '?', 'Mexico', 'South',
+                            'Puerto-Rico', 'Honduras', 'England', 'Canada', 'Germany', 'Iran',
+                            'Philippines', 'Italy', 'Poland', 'Columbia', 'Cambodia', 'Thailand', 'Ecuador',
+                            'Laos', 'Taiwan', 'Haiti', 'Portugal', 'Dominican-Republic', 'El-Salvador',
                             'France', 'Guatemala', 'China', 'Japan', 'Yugoslavia', 'Peru',
-                            'Outlying-US(Guam-USVI-etc)' ,'Scotland', 'Trinadad&Tobago', 'Greece',
-                            'Nicaragua', 'Vietnam', 'Hong' ,'Ireland' ,'Hungary', 'Holand-Netherlands']\
-                                  = Field(alias="native-country")
+                            'Outlying-US(Guam-USVI-etc)', 'Scotland', 'Trinadad&Tobago', 'Greece',
+                            'Nicaragua', 'Vietnam', 'Hong', 'Ireland', 'Hungary', 'Holand-Netherlands']\
+        = Field(alias="native-country")
 
 
 app = FastAPI()
 
 # This allows sending of data (our TaggedItem) via POST to the API.
-print (f"{folder_path}/model/")
+print(f"{folder_path}/model/")
+
+
 def get_model():
-    print (os.listdir())
-    return joblib.load(f"{folder_path}/model/model.joblib"), joblib.load(f"{folder_path}/model/encoder.joblib"),joblib.load(f"{folder_path}/model/lb.joblib")
+    print(os.listdir())
+    return joblib.load(f"{folder_path}/model/model.joblib"), joblib.load(
+        f"{folder_path}/model/encoder.joblib"), joblib.load(f"{folder_path}/model/lb.joblib")
+
 
 model, encoder, lb = get_model()
 
+
 @app.get("/")
 async def greetings():
-    return {"message":"hello world"}
+    return {"message": "hello world"}
+
 
 @app.post("/predict/")
 async def predict_salary(item: InputItem):
     cat_features = [
-    "workclass",
-    "education",
-    "marital_status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native_country",
+        "workclass",
+        "education",
+        "marital_status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native_country",
     ]
 
+    print(item)
+    df = pd.DataFrame.from_dict([item.dict()], orient="columns")
 
-    print (item)
-    df = pd.DataFrame.from_dict([item.dict()], orient = "columns")
-  
-    data = process_data(df, encoder = encoder, lb = lb, label = None, categorical_features = cat_features, training = False)[0]
+    data = process_data(
+        df,
+        encoder=encoder,
+        lb=lb,
+        label=None,
+        categorical_features=cat_features,
+        training=False)[0]
 
     prediction = inference(model, data)[0]
     prediction = str(lb.inverse_transform(prediction)[0])
--- starter/train_model.py
+++ starter/train_model.py
@@ -21,16 +21,21 @@ cat_features = [
     "native-country",
 ]
 
+
 def slice_inference(data, features, model, encoder, lb):
     r = {}
     for feature in features:
         for value in data[feature].unique():
             subset = data[data[feature] == value]
-            X, y, _, __ = process_data(subset,categorical_features=features, training = False, encoder = encoder, lb = lb,label="salary")
+            X, y, _, __ = process_data(
+                subset, categorical_features=features, training=False, encoder=encoder, lb=lb, label="salary")
             pred = model.predict(X)
             precision, recall, fscore = compute_model_metrics(y, pred)
             if feature in r.keys():
-                r[feature][value] = {"precision" : precision, "recall" : recall, "fscore" : fscore}
+                r[feature][value] = {
+                    "precision": precision,
+                    "recall": recall,
+                    "fscore": fscore}
             else:
                 r[feature] = {}
     return r
@@ -38,24 +43,24 @@ def slice_inference(data, features, model, encoder, lb):
 
 if __name__ == "__main__":
 
-    data = pd.read_csv(f"{folder_path}/data/census_cleaned.csv")    
+    data = pd.read_csv(f"{folder_path}/data/census_cleaned.csv")
     train, test = train_test_split(data, test_size=0.20, random_state=42)
     X_train, y_train, encoder, lb = process_data(
         train, categorical_features=cat_features, label="salary", training=True
     )
 
-    X_test, y_test, encoder, lb = process_data(test,categorical_features=cat_features, training = False, encoder = encoder, lb = lb,label="salary")
+    X_test, y_test, encoder, lb = process_data(
+        test, categorical_features=cat_features, training=False, encoder=encoder, lb=lb, label="salary")
 
     model = train_model(X_train, y_train)
-    predictions = inference(model , X_test)
+    predictions = inference(model, X_test)
 
-    print (compute_model_metrics(y_test, predictions))
+    print(compute_model_metrics(y_test, predictions))
 
     joblib.dump(model, f"{folder_path}/model/model.joblib")
     joblib.dump(encoder, f"{folder_path}/model/encoder.joblib")
     joblib.dump(lb, f"{folder_path}/model/lb.joblib")
 
-    r = slice_inference(test, cat_features,  model, encoder, lb)
-    with open (f"{folder_path}/model/slice_inference.json", "w") as file:
-        json.dump(r, file, indent = 4)
-
+    r = slice_inference(test, cat_features, model, encoder, lb)
+    with open(f"{folder_path}/model/slice_inference.json", "w") as file:
+        json.dump(r, file, indent=4)
